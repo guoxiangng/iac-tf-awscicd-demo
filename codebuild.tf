3e32a1121627b4cd943800c4bf5aabbe30210a6f
Ng Guo Xiang  (NCS) <guo.xiang.ng1@ncs.com.sg>
2024-07-03 22:19:47 +0800

    Added cross account codebuild permissions. Updated codepipeline configs

--- codebuild.tf
+++ codebuild.tf
@@ -58,7 +58,37 @@ resource "aws_iam_role" "codebuild" {
   tags = merge(var.additional_tags, )
 }
 
+#Attach policy with current account deployment permissions
 resource "aws_iam_role_policy_attachment" "codebuild" {
   role       = aws_iam_role.codebuild.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+#Attach policy with acc1 (cross account) deployment permissions
+resource "aws_iam_role_policy_attachment" "codebuild2" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = aws_iam_policy.assume_role_policy_acc1deploy.arn
+}
+
+resource "aws_iam_policy" "assume_role_policy_acc1deploy" {
+  name        = "assume_role_policy_for_acc_395890110782"
+  description = "Assume Role permission for AccountId 395890110782"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": "arn:aws:iam::395890110782:role/lzdemo-crossacc-tfdeploy",
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "lzdemo"
+        }
+      }
+    }
+  ]
+}
+EOF
 }
